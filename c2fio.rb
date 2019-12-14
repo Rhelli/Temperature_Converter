c9ec9e6fe00a9683120bd9663380a785c7898bda
Rory Hellier <rory.hellier@googlemail.com>
2019-12-14 15:12:04 -0500

    Updated converter to accept user_input

--- c2fio.rb
+++ c2fio.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-celcius =  100
-fahrenheit = (celcius * 9 / 5) + 32
+print 'Hello. Please enter a Celsius value: '
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
 print 'The result is '
 print fahrenheit
 puts '.'
